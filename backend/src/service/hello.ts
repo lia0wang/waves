fc905986d75e68136d2131c2f86bd4a5882c602a
lia0wang <lia0wang@outlook.com>
2023-03-29 09:56:11 +1100

    delete

--- backend/src/service/hello.ts
+++
@@ -1,9 +0,0 @@
-import { trpc } from "../trpc_provider"
-
-const helloRouter = trpc.router({
-  helloWorld: trpc.procedure.query(() => {
-    return "Hello World"
-  })
-})
-
-export default helloRouter
